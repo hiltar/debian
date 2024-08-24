167e78132bca1bae3dbc29caaa59918ad2a523fe
hiltar <43303122+hiltar@users.noreply.github.com>
2024-08-24 14:06:50 +0300

    Create flatpaks.sh

---
+++ scripts/flatpaks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  flatpak install flathub com.brave.Browser
+  flatpak install flathub com.github.tchx84.Flatseal
+  flatpak install flathub org.mozilla.firefox
+  flatpak install flathub org.telegram.desktop
+  flatpak install flathub org.telegram.desktop.webview
+else
+  echo "Run as non-root"
+fi
