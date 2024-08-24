5c0b6feebb6c24ecbe37fe99395edef15d5fee4b
hiltar <43303122+hiltar@users.noreply.github.com>
2024-08-24 14:27:50 +0300

    Add noninteractive installations

--- scripts/flatpaks.sh
+++ scripts/flatpaks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ] ; then
-  flatpak install flathub com.brave.Browser
-  flatpak install flathub com.github.tchx84.Flatseal
-  flatpak install flathub org.mozilla.firefox
-  flatpak install flathub org.telegram.desktop
-  flatpak install flathub org.telegram.desktop.webview
+  flatpak install -y --noninteractive flathub com.brave.Browser
+  flatpak install -y --noninteractive flathub com.github.tchx84.Flatseal
+  flatpak install -y --noninteractive flathub org.mozilla.firefox
+  flatpak install -y --noninteractive flathub org.telegram.desktop
+  flatpak install -y --noninteractive flathub org.telegram.desktop.webview
 else
   echo "Run as non-root"
 fi
