7f6b950c518d751afebd4c0971537e9654210f89
hiltar <43303122+hiltar@users.noreply.github.com>
2024-08-26 21:20:39 +0300

    Update flatpaks.sh

--- scripts/flatpaks.sh
+++ scripts/flatpaks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ] ; then
-  flatpak install -y --noninteractive flathub com.brave.Browser
+  flatpak install -y --noninteractive flathub org.chromium.Chromium
   flatpak install -y --noninteractive flathub com.github.tchx84.Flatseal
   flatpak install -y --noninteractive flathub org.mozilla.firefox
   flatpak install -y --noninteractive flathub org.telegram.desktop
